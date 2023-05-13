d94bf2c7d02d300a5fcb539daa2941f2c45bb7ca
Evan Weiss <evanweissaz@gmail.com>
2023-05-12 21:21:45 -0400

    more

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
 let copyrightElement = document.getElementById("copyright");
 let currentYear = new Date().getFullYear();
 copyrightElement.innerHTML += " " + currentYear;
+
+let greetingElement = document.getElementById("greeting");
+let currentHour = new Date().getHours();
+let greeting;
+if (currentHour < 12) {
+    greeting = "Good morning!";
+} else if (currentHour < 18) {
+    greeting = "Good afternoon!";
+} else {
+    greeting = "Good evening!";
+}
+greetingElement.textContent = greeting;
