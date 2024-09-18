cd7e33a1d7bbccefb55b181b0915e16b3cfe07bb
Evan Weiss <evanweissaz@gmail.com>
2024-09-18 13:06:19 -0400

    comitt

--- main.js
+++ main.js
@@ -1,17 +1,7 @@
-let copyrightElement = document.getElementById("copyright");
-let currentYear = new Date().getFullYear();
-copyrightElement.innerHTML += " " + currentYear;
-
 let dateTimeElement = document.getElementById("datetime");
 
 function updateDateTime() {
     let now = new Date();
-    dateTimeElement.textContent = now.toLocaleString();
+    dateTimeElement.textContent = "Current Date and Time: " + now.toLocaleString();
 }
 setInterval(updateDateTime, 1000);
-
-// Dark mode toggle logic
-let darkModeToggle = document.getElementById("darkModeToggle");
-darkModeToggle.addEventListener("click", function() {
-    document.body.classList.toggle("dark-mode");
-});
